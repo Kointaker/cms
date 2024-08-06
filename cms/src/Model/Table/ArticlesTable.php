d5254281e69736ca26eb6d5474ca3515e8aaaff3
Bko Cuevas <bko@Bkos-MacBook-Air.local>
2024-08-06 14:59:54 -0400

    More functionality has been added (delete, validation)

--- cms/src/Model/Table/ArticlesTable.php
+++ cms/src/Model/Table/ArticlesTable.php
@@ -2,11 +2,15 @@
 // src/Model/Table/ArticlesTable.php
 namespace App\Model\Table;
 
+
+
+
 use Cake\ORM\Table;
 // the Text class
 use Cake\Utility\Text;
 // the EventInterface class
 use Cake\Event\EventInterface;
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table
 {
@@ -23,5 +27,15 @@ class ArticlesTable extends Table
             $entity->slug = substr($sluggedTitle, 0, 191);
         }
     }
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
 
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+        return $validator;
+        }
 }
