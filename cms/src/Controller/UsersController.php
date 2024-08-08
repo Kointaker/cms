f5cb56503fc4bc0744c8baa9909872992fb8dd1c
Bko Cuevas <bko@Bkos-MacBook-Air.local>
2024-08-08 10:32:11 -0400

    Authentication finished (Logout and Enabling resgistration)

--- cms/src/Controller/UsersController.php
+++ cms/src/Controller/UsersController.php
@@ -103,7 +103,7 @@ class UsersController extends AppController
         parent::beforeFilter($event);
         // Configuring the login action to not require authentication, preventing
         // the infinite redirect loop issue
-        $this->Authentication->addUnauthenticatedActions(['login']);
+        $this->Authentication->addUnauthenticatedActions(['login', 'add']);
     }
 
     public function login()
@@ -125,7 +125,18 @@ class UsersController extends AppController
             $this->Flash->error(__('Invalid username or password'));
         }
     }
+    
+    public function logout()
+    {
+        $result = $this->Authentication->getResult();
+        // regardless of POST or GET, redirect if user is logged in
+        if ($result && $result->isValid()) {
+            $this->Authentication->logout();
 
+            return $this->redirect(['controller' => 'Users', 'action' => 'login']);
+
+        }
+    }
 
 
 
